a3da72d265a29b3f4f2915bb1191040a27cc3b68
Mohammad Alfrad Nobel Bhuiyan <nobel1154@gmail.com>
2019-05-31 15:47:04 -0400

    updated

--- deprivation_index_by_zipcode.r
+++ deprivation_index_by_zipcode.r
@@ -1,20 +1,17 @@
 library(tidyverse)
 
-# read deprivation index
-library(tidyverse)
-
 # Deprivation index
 dep_index <- readRDS(gzcon(url('https://github.com/cole-brokamp/dep_index/raw/master/ACS_deprivation_index_by_census_tracts.rds'))) 
 
 
 # ZCTA data 
 ZCTA_data<- read.table("https://www2.census.gov/geo/docs/maps-data/data/rel/zcta_tract_rel_10.txt", header = TRUE,sep = ',',
-                       colClasses = c('ZCTA5' = 'character' , 'GEOID' = 'character'))
+                       colClasses = c('ZCTA5' = 'character' , 'GEOID' = 'character')) %>%
+   dplyr::select(ZCTA5,GEOID)
 
 
 # combine two data 
 ZCTA_data <- dplyr::left_join(dep_index,ZCTA_data, by= c('census_tract_fips' = 'GEOID'))
-
 
 # Deprivation index by Zip code
 ZCTA_data<- ZCTA_data %>%
@@ -23,3 +20,6 @@ ZCTA_data<- ZCTA_data %>%
 
 write.csv(ZCTA_data, file= 'ACS_deprivation_index_by_zipcode.csv', row.names = FALSE)
 saveRDS(ZCTA_data, "ACS_deprivation_index_by_zipcode.rds")
+
+
+
